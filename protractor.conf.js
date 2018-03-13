df1dd6b128026f37a3b358918bfd7e2346b0ac4e
FollowTheFlo <florent.letendre@hotmail.fr>
2018-03-13 01:41:21 -0400

    add firefox in e2e

--- protractor.conf.js
+++ protractor.conf.js
@@ -8,9 +8,11 @@ exports.config = {
   specs: [
     './e2e/**/*.e2e-spec.ts'
   ],
-  capabilities: {
+  multiCapabilities: [{
+    'browserName': 'firefox'
+  }, {
     'browserName': 'chrome'
-  },
+  }],
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
   framework: 'jasmine',
